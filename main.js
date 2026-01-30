2d82abe2092342b947d9881ab02289d9574b40c0
tigranmamikonyan08 <tigranmamikonyan08@gmail.com>
2026-01-30 17:40:51 +0400

    bug fix

--- main.js
+++ main.js
@@ -2,24 +2,38 @@ const calculator = document.querySelector('.bottom')
 const result = document.querySelector('.result')
 const minCalc = document.querySelector('.minCalc')
 
-calculator.addEventListener('click',(e)=>{
-    if (/\d/.test(e.target.textContent) && e.target.textContent.length < 2){
+function isNumber(str){
+    return str !== '' && !isNaN(str)
+}
+
+function removeOperators(str){
+    let newStr = ''
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ' && !isNaN(str[i])) {
+            newStr += str[i]
+        }
+    }
+    return newStr
+}
+
+calculator.addEventListener('click',(e)=>{    
+    if (isNumber(e.target.textContent) && e.target.textContent.length < 2){
         if (minCalc.textContent.includes('=')) {            
             minCalc.textContent = ''
-            result.textContent = /\d/.test(e.target.textContent) ? e.target.textContent : result.textContent   
+            result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent   
         }else{
             result.textContent += e.target.textContent            
         }
     }
-    if ((minCalc.textContent.slice(-1) === '+' || minCalc.textContent.slice(-1) === '=') && +minCalc.textContent.replace(/[+=]/g,'') === +result.textContent.slice(0,-1)){
-        result.textContent = /\d/.test(e.target.textContent) ? e.target.textContent : result.textContent
+    if ((minCalc.textContent.slice(-1) === '+' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === +result.textContent.slice(0,-1)){
+        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent
         
     }
     if(e.target.textContent === '+'){
         if(minCalc.textContent === '' && e.target.textContent === '+' && result.textContent === '') {
         } else if(minCalc.textContent.includes('=') && e.target.textContent === '+') {
-            minCalc.textContent = `${result.textContent} +` 
-            console.log('kkk');
+            minCalc.textContent = `${result.textContent} +`
 
         } else if(minCalc.textContent.trim().slice(-1) === '-' || minCalc.textContent.trim().slice(-1) === '*' || minCalc.textContent.trim().slice(-1) === '/'){
             let current = e.target.textContent
@@ -44,15 +58,15 @@ calculator.addEventListener('click',(e)=>{
 
 // ------------------------------------------------------------------------------------------------------------------------
 
-if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === '=') && +minCalc.textContent.replace(/[*=]/g,'') === +result.textContent.slice(0,-1)){
-        result.textContent = /\d/.test(e.target.textContent) ? e.target.textContent : result.textContent
+if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === +result.textContent.slice(0,-1)){
+        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent
     }
     if(e.target.textContent === '*' && !isNaN(result.textContent)){
         if(minCalc.textContent === '' && e.target.textContent === '*' && result.textContent === '') {
             return
         } else if(minCalc.textContent.includes('=') && e.target.textContent === '*') {
             minCalc.textContent = `${result.textContent} *` 
-            
+
         } else if(minCalc.textContent.trim().slice(-1) === '-' || minCalc.textContent.trim().slice(-1) === '+' || minCalc.textContent.trim().slice(-1) === '/'){
             let current = e.target.textContent
             let last = minCalc.textContent.slice(-1)
@@ -75,8 +89,8 @@ if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === 
     // -------------------------------------------------------------------------------------------------------------------------------
 
     let plusKamMinus = +result.textContent.slice(0,-1) > 0 ? +result.textContent.slice(0,-1) : +(-result.textContent.slice(0,-1))
-    if ((minCalc.textContent.slice(-1) === '-' || minCalc.textContent.slice(-1) === '=') && +minCalc.textContent.replace(/[-=]/g,'') === plusKamMinus){
-        result.textContent = /\d/.test(e.target.textContent) ? e.target.textContent : result.textContent             
+    if ((minCalc.textContent.slice(-1) === '-' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === plusKamMinus){
+        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent             
     }
     if(e.target.textContent === '-' && !isNaN(result.textContent)){
         if(minCalc.textContent === '' && e.target.textContent === '-' && result.textContent === '') {
@@ -90,25 +104,25 @@ if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === 
 
             minCalc.textContent = minCalc.textContent.slice(0,j) + current + minCalc.textContent.slice(j + 1)
         } else {
-            let minusOrPlus = minCalc.textContent.replace(/-(?=[^-]*$)/, '') >= 0 ? +result.textContent : -result.textContent
+            let minusOrPlus = removeOperators(minCalc.textContent) >= 0 ? +result.textContent : -result.textContent
             
             minCalc.textContent = +minCalc.textContent.replace('-','').trim() ?
-                 +minCalc.textContent.replace(/-(?=[^-]*$)/, '').trim()  - +result.textContent + ' -' : 
-                    +minCalc.textContent.replace(/-(?=[^-]*$)/, '').trim()  + minusOrPlus + ' -'
-            result.textContent = minCalc.textContent.replace(/-(?=[^-]*$)/, '')
+                 +removeOperators(minCalc.textContent)  - +result.textContent + ' -' : 
+                    +removeOperators(minCalc.textContent)  + minusOrPlus + ' -'
+            result.textContent = removeOperators(minCalc.textContent)
         }
     }
 
     if (e.target.textContent === '=' && !(minCalc.textContent.at(-1) === '=') && minCalc.textContent !== '' && minCalc.textContent.includes('-')) {    
-        const res = +minCalc.textContent.replace(/-(?=[^-]*$)/, '') - +result.textContent
+        const res = +removeOperators(minCalc.textContent) - +result.textContent
         minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
         result.textContent = res
     }
 
     // --------------------------------------------------------------------------------------------------------------------------------
 
-    if ((minCalc.textContent.slice(-1) === '/' || minCalc.textContent.slice(-1) === '=') && +minCalc.textContent.replace(/[/=]/g,'') === +result.textContent.slice(0,-1)){
-        result.textContent = /\d/.test(e.target.textContent) ? e.target.textContent : result.textContent
+    if ((minCalc.textContent.slice(-1) === '/' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === +result.textContent.slice(0,-1)){
+        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent
     }
 
     if(e.target.textContent === '/' && !isNaN(result.textContent)){
@@ -148,7 +162,6 @@ if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === 
         }else{
             result.textContent = `-${result.textContent}`
         }
-        
     }
 
     if (e.target.textContent === 'C') {
