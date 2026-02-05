67e69d786082b1121cb89dbef4457c5692ab1ade
tigranmamikonyan08 <tigranmamikonyan08@gmail.com>
2026-02-05 14:22:44 +0400

    decimal-calc

--- main.js
+++ main.js
@@ -17,6 +17,21 @@ function removeOperators(str){
     return newStr
 }
 
+function createOperator(str){
+    let arr = str.split(' ')
+
+    switch(arr[1]) {
+        case '+':
+            return +arr[0] + +arr[2]
+        case '-':
+            return +arr[0] - +arr[2]
+        case '*':
+            return +arr[0] * +arr[2]
+        case '/':
+            return +arr[0] / +arr[2]
+    }
+}
+
 function removeLastMinus(str){
     let idx = str.lastIndexOf('-')
     if (idx === -1) {
@@ -27,133 +42,235 @@ function removeLastMinus(str){
     return newStr
 }
 
+function binaryToDecimal(str){
+    let res 
+    let arr = str.split(' ')
+    let n = arr[0].split('.')[1].length
+    let m = arr[2].split('.')[1].length
+    let operator = arr[1]
+
+    switch(operator){
+        case '+':
+            return res = ((arr[0] * 10**n + arr[2] * 10**m) / 10**n)
+        case '-':
+            return res = ((arr[0] * 10**n - arr[2] * 10**m) / 10**n)
+        case '*':
+            return res = (arr[0] * 10**n) * (arr[2] * 10**m) / 10**(n+m)
+        case '/':
+            if (n >= m) {
+                return res = (arr[0] * 10**n) / (arr[2] * 10**m) / 10**(n-m)
+            }else{
+                return res = (arr[0] * 10**n) / (arr[2] * 10**m) * 10**(m-n)
+            }
+    }
+    return res
+}
+
 calculator.addEventListener('click',(e)=>{
     if (isNumber(e.target.textContent) && e.target.textContent.length < 2){
         if (e.target.textContent === '0' && result.textContent === '0'){        
             return
         }
-        if (minCalc.textContent.includes('=') || result.textContent === '0') {            
+        if (minCalc.textContent.includes('=')) {            
             minCalc.textContent = ''
             result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent
         }else{
-            result.textContent += e.target.textContent            
+            if (result.textContent == 0 && result.textContent.slice(-1) != '.') {
+                result.textContent = e.target.textContent
+            }else{
+                result.textContent += e.target.textContent
+            }         
         }
     }
-        
-    if ((minCalc.textContent.slice(-1) === '+' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === +result.textContent.slice(0,-1)){
-        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent
+
+    if ((minCalc.textContent.slice(-1) === '+' || minCalc.textContent.slice(-1) === '=')){
+        if (!(+result.textContent) && result.textContent != 0){
+            result.textContent = e.target.textContent
+        }
     }
     if(e.target.textContent === '+' && !isNaN(result.textContent)){
         if(minCalc.textContent === '' && e.target.textContent === '+' && result.textContent === '') {
             return
         } else if(minCalc.textContent.includes('=') && e.target.textContent === '+') {
-            minCalc.textContent = `${result.textContent} +`
+            minCalc.textContent = `${result.textContent} +`            
+            result.textContent = result.textContent + ' '  
+            console.log(minCalc.textContent);
         } else if(minCalc.textContent.trim().slice(-1) === '-' || minCalc.textContent.trim().slice(-1) === '*' || minCalc.textContent.trim().slice(-1) === '/'){
-            let current = e.target.textContent
-            let last = minCalc.textContent.slice(-1)
-            let j = minCalc.textContent.lastIndexOf(last)            
-
-            minCalc.textContent = minCalc.textContent.slice(0,j) + current + minCalc.textContent.slice(j + 1)
-        } else {            
-            minCalc.textContent = +minCalc.textContent.replace('+','') + +result.textContent + ' +'
-            result.textContent = minCalc.textContent.replace('+','')            
+            let currentOp = e.target.textContent
+            let onGoingOp = minCalc.textContent.slice(-1)            
+            let arr = minCalc.textContent.split(' ')
+            if (arr[0] > 0) {
+                minCalc.textContent = createOperator(`${+minCalc.textContent.replace(onGoingOp,'').trim()} ${onGoingOp} ${+result.textContent} `) + ' ' + currentOp
+            }else{
+                let idx = minCalc.textContent.lastIndexOf(onGoingOp)
+                minCalc.textContent = createOperator(`${+minCalc.textContent.slice(0,idx)} ${onGoingOp} ${+result.textContent} `) + ' ' + currentOp
+            }
+            result.textContent = minCalc.textContent.replace('+', ' ')
+        } else {   
+            if (minCalc.textContent.includes('.') && result.textContent.includes('.')) {
+                let str = `${minCalc.textContent} ${result.textContent}`
+                minCalc.textContent = binaryToDecimal(str) + ' +'
+            }else{
+                minCalc.textContent = +minCalc.textContent.replace('+','') + +result.textContent + ' +'
+            }
+            result.textContent = minCalc.textContent.replace('+','')
         }
     }
     
     if (e.target.textContent === '=' && !(minCalc.textContent.at(-1) === '=') && minCalc.textContent !== '' && minCalc.textContent.includes('+')){
-        
-        let res = +minCalc.textContent.replace('+', '') + +result.textContent
-        
-        minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
-        result.textContent = res
+        if (minCalc.textContent.replace('+', '').includes('.') && result.textContent.includes('.')) {
+            let str = `${minCalc.textContent} ${result.textContent}`
+            let res = binaryToDecimal(str)
+            minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
+            result.textContent = res
+        }else{
+            let res = +minCalc.textContent.replace('+', '') + +result.textContent
+            minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
+            result.textContent = res
+        }
     }
 
 // ------------------------------------------------------------------------------------------------------------------------
 
-if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === +result.textContent.slice(0,-1)){
-        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent
+if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === '=') && isNumber(e.target.textContent)){
+        if (!(+result.textContent)){
+            result.textContent = e.target.textContent
+        }
     }
     if(e.target.textContent === '*' && !isNaN(result.textContent)){
         if(minCalc.textContent === '' && e.target.textContent === '*' && result.textContent === '') {
             return
         } else if(minCalc.textContent.includes('=') && e.target.textContent === '*') {
             minCalc.textContent = `${result.textContent} *` 
-            
+            result.textContent = result.textContent + ' '
         } else if(minCalc.textContent.trim().slice(-1) === '-' || minCalc.textContent.trim().slice(-1) === '+' || minCalc.textContent.trim().slice(-1) === '/'){
-            let current = e.target.textContent
-            let last = minCalc.textContent.slice(-1)
-            let j = minCalc.textContent.lastIndexOf(last)            
+            let currentOp = e.target.textContent
+            let onGoingOp = minCalc.textContent.slice(-1)
+            
+            let arr = minCalc.textContent.split(' ')
 
-            minCalc.textContent = minCalc.textContent.slice(0,j) + current + minCalc.textContent.slice(j + 1)    
-                    
-        } else {            
-            minCalc.textContent = +minCalc.textContent.replace('*','') ?
-                 +minCalc.textContent.replace('*','')  * +result.textContent + ' *' : 
-                    +minCalc.textContent.replace('*','')  + +result.textContent + ' *'
+            if (arr[0] > 0) {
+                minCalc.textContent = createOperator(`${+minCalc.textContent.replace(onGoingOp,'').trim()} ${onGoingOp} ${+result.textContent} `) + ' ' + currentOp
+            }else{
+                let idx = minCalc.textContent.lastIndexOf(onGoingOp)
+                minCalc.textContent = createOperator(`${+minCalc.textContent.slice(0,idx)} ${onGoingOp} ${+result.textContent} `) + ' ' + currentOp
+            }
+            result.textContent = minCalc.textContent.replace('*', ' ')       
+        } else {
+            if (minCalc.textContent === '') {
+                minCalc.textContent = +result.textContent + ' *'
+            }else{
+                if (minCalc.textContent.includes('.') && result.textContent.includes('.')) {
+                    let str = `${minCalc.textContent} ${result.textContent}`
+                    minCalc.textContent = binaryToDecimal(str) + ' *'   
+                }else{
+                    minCalc.textContent = result.textContent * minCalc.textContent.replace('*','') + ' *'
+                }
+            }
             result.textContent = minCalc.textContent.replace('*','')
         }
     }
     if (e.target.textContent === '=' && !(minCalc.textContent.at(-1) === '=') && minCalc.textContent !== '' && minCalc.textContent.includes('*')) {
-        let res = +minCalc.textContent.replace('*', '') * +result.textContent
-        minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
-        result.textContent = res
+        
+        if (minCalc.textContent.replace('*','').includes('.') && result.textContent.includes('.')) {
+            let str = `${minCalc.textContent} ${result.textContent}`
+            let res = binaryToDecimal(str)
+            minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
+            result.textContent = res
+        }else{
+            let res = +minCalc.textContent.replace('*', '') * +result.textContent
+            minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
+            result.textContent = res
+        }
     }
-
     // -------------------------------------------------------------------------------------------------------------------------------
 
     let plusKamMinus = +result.textContent.slice(0,-1) > 0 ? +result.textContent.slice(0,-1) : +result.textContent.slice(0,-1)    
-    if ((minCalc.textContent.slice(-1) === '-' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === plusKamMinus){
-        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent                 
+    if ((minCalc.textContent.slice(-1) === '-' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === plusKamMinus){        
+        if (!(+result.textContent)){
+            result.textContent = e.target.textContent
+        }
     }
     if(e.target.textContent === '-' && !isNaN(result.textContent)){
         if(minCalc.textContent === '' && e.target.textContent === '-' && result.textContent === '') {
             return
         } else if(minCalc.textContent.includes('=') && e.target.textContent === '-') {
-            minCalc.textContent = `${result.textContent} -` 
+            minCalc.textContent = `${result.textContent} -`
+            result.textContent = result.textContent + ' '
         } else if(minCalc.textContent.trim().slice(-1) === '+' || minCalc.textContent.trim().slice(-1) === '*' || minCalc.textContent.trim().slice(-1) === '/'){                 
-            let current = e.target.textContent
-            let last = minCalc.textContent.slice(-1)
-            let j = minCalc.textContent.lastIndexOf(last)            
-
-            minCalc.textContent = minCalc.textContent.slice(0,j) + current + minCalc.textContent.slice(j + 1)
+            let currentOp = e.target.textContent
+            let onGoingOp = minCalc.textContent.slice(-1)            
+            minCalc.textContent = createOperator(`${+minCalc.textContent.replace(onGoingOp,'').trim()} ${onGoingOp} ${+result.textContent} `) + ' ' + currentOp
+            let secondMinus = minCalc.textContent.lastIndexOf('-')
+            result.textContent = minCalc.textContent.slice(0,secondMinus) + minCalc.textContent.slice(secondMinus + 1)
         } else {
             let minusOrPlus = removeLastMinus(minCalc.textContent) >= 0 ? +result.textContent : -result.textContent
 
-            minCalc.textContent = +minCalc.textContent.replace('-','').trim() ?
+            if (minCalc.textContent.includes('.') && result.textContent.includes('.')) {
+                let str = `${minCalc.textContent} ${result.textContent}`
+                minCalc.textContent = binaryToDecimal(str) + ' -'
+                result.textContent = removeLastMinus(minCalc.textContent) 
+                
+            }else{
+                minCalc.textContent = +minCalc.textContent.replace('-','').trim() ?
                  +removeLastMinus(minCalc.textContent)  - +result.textContent + ' -' : 
                     +removeLastMinus(minCalc.textContent)  + minusOrPlus + ' -'
-            result.textContent = removeLastMinus(minCalc.textContent)            
+                    
+                result.textContent = removeLastMinus(minCalc.textContent) 
+            }
         }
     }
 
     if (e.target.textContent === '=' && minCalc.textContent.at(-1) !== '=' && minCalc.textContent !== '' && minCalc.textContent.includes('-') &&  !minCalc.textContent.includes('/')) {    
-        let res = +removeLastMinus(minCalc.textContent) - +result.textContent
-        minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
-        result.textContent = res
+        if (+removeLastMinus(minCalc.textContent).includes('.') && result.textContent.includes('.')) {
+            let str = `${minCalc.textContent} ${result.textContent}`
+            let res = binaryToDecimal(str)
+            minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
+            result.textContent = res
+        }else{
+            let res = +removeLastMinus(minCalc.textContent) - +result.textContent
+            minCalc.textContent = `${minCalc.textContent} ${result.textContent} =`
+            result.textContent = res
+        }
     }
 
     // --------------------------------------------------------------------------------------------------------------------------------
 
-    if ((minCalc.textContent.slice(-1) === '/' || minCalc.textContent.slice(-1) === '=') && +removeOperators(minCalc.textContent) === +result.textContent.slice(0,-1)){
-        result.textContent = isNumber(e.target.textContent) ? e.target.textContent : result.textContent        
+    if ((minCalc.textContent.slice(-1) === '/' || minCalc.textContent.slice(-1) === '=') && isNumber(e.target.textContent)){
+        if (!(+result.textContent)){
+            result.textContent = e.target.textContent
+        }   
     }
 
     if(e.target.textContent === '/' && !isNaN(result.textContent)){
         if(minCalc.textContent === '' && e.target.textContent === '/' && result.textContent === '') {
             return
         }else if(minCalc.textContent.includes('=') && e.target.textContent === '/') {
-            minCalc.textContent = `${result.textContent} /`             
+            minCalc.textContent = `${result.textContent} /`
+            result.textContent = result.textContent + ' '
         } else if(minCalc.textContent.trim().slice(-1) === '+' || minCalc.textContent.trim().slice(-1) === '*' || minCalc.textContent.trim().slice(-1) === '-'){
-            let current = e.target.textContent
-            let last = minCalc.textContent.slice(-1)
-            let j = minCalc.textContent.lastIndexOf(last)            
+            let currentOp = e.target.textContent
+            let onGoingOp = minCalc.textContent.slice(-1)
+            let arr = minCalc.textContent.split(' ')
 
-            minCalc.textContent = minCalc.textContent.slice(0,j) + current + minCalc.textContent.slice(j + 1)
+            if (arr[0] > 0) {
+                minCalc.textContent = createOperator(`${+minCalc.textContent.replace(onGoingOp,'').trim()} ${onGoingOp} ${+result.textContent} `) + ' ' + currentOp
+            }else{
+                let idx = minCalc.textContent.lastIndexOf(onGoingOp)
+                minCalc.textContent = createOperator(`${+minCalc.textContent.slice(0,idx)} ${onGoingOp} ${+result.textContent} `) + ' ' + currentOp
+            }
+            result.textContent = minCalc.textContent.replace('/', ' ')
         } else{
-            minCalc.textContent = +minCalc.textContent.replace('/','') ?
+            if (minCalc.textContent.includes('.') && result.textContent.includes('.')) {
+                let str = `${minCalc.textContent} ${result.textContent}`
+                minCalc.textContent = binaryToDecimal(str) + ' /'
+                result.textContent = minCalc.textContent.replace('/','')
+            }else{
+                minCalc.textContent = +minCalc.textContent.replace('/','') ?
                  +minCalc.textContent.replace('/','')  / +result.textContent + ' /' : 
                     +minCalc.textContent.replace('/','')  + +result.textContent + ' /'
-            result.textContent = minCalc.textContent.replace('/','')
+                result.textContent = minCalc.textContent.replace('/','')
+            }
         }
     }
 
@@ -165,8 +282,8 @@ if ((minCalc.textContent.slice(-1) === '*' || minCalc.textContent.slice(-1) === 
     }
 
     if (e.target.textContent === ',') {
-        if (!result.textContent.includes('.')) {
-            result.textContent = result.textContent.slice(0) + '.'
+        if (!result.textContent.includes('.') && result.textContent != '') {
+            result.textContent = result.textContent + '.'
         }
     }
 
